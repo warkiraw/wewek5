8dfb05f497a95582948d4615ef4a9ac6c2064cb9
warkiraw <120770226+warkiraw@users.noreply.github.com>
2022-12-30 10:03:35 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,121 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+document.querySelector('#push2').onclick = function(){
+    if(document.querySelector('#newtask2 input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks2').innerHTML += `
+            <div class="task2">
+                <span id="taskname2">
+                    ${document.querySelector('#newtask2 input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+document.querySelector('#push3').onclick = function(){
+    if(document.querySelector('#newtask3 input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks3').innerHTML += `
+            <div class="task3">
+                <span id="taskname3">
+                    ${document.querySelector('#newtask3 input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+document.querySelector('#push4').onclick = function(){
+    if(document.querySelector('#newtask4 input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks4').innerHTML += `
+            <div class="task4">
+                <span id="taskname4">
+                    ${document.querySelector('#newtask4 input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+document.querySelector('#push5').onclick = function(){
+    if(document.querySelector('#newtask5 input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks5').innerHTML += `
+            <div class="task5">
+                <span id="taskname5">
+                    ${document.querySelector('#newtask5 input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+document.querySelector('#push6').onclick = function(){
+    if(document.querySelector('#newtask6 input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks6').innerHTML += `
+            <div class="task6">
+                <span id="taskname6">
+                    ${document.querySelector('#newtask6 input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+document.querySelector('#push7').onclick = function(){
+    if(document.querySelector('#newtask7 input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks7').innerHTML += `
+            <div class="task7">
+                <span id="taskname7">
+                    ${document.querySelector('#newtask7 input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+document.querySelector('#push8').onclick = function(){
+    if(document.querySelector('#newtask8 input').value.length == 0){
+        alert("Пожалуйста оцените")
+    }
+
+    else{
+        document.querySelector('#tasks8').innerHTML += `
+            <div class="task8">
+                <span id="taskname8">
+                    ${document.querySelector('#newtask8 input').value}
+                </span>
+            </div>
+        `;
+    }
+}
+    
